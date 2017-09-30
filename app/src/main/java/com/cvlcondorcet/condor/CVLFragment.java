30f1f581158b274e852626b66760cf98be037302
Quentin DE MUYNCK <quentin.demuynck@outlook.com>
2017-09-30 23:10:58 +0200

    Adding CVLFragment (no tests)

---
+++ app/src/main/java/com/cvlcondorcet/condor/CVLFragment.java
@@ -0,0 +1,33 @@
+package com.cvlcondorcet.condor;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+/**
+ * Created by Quentin DE MUYNCK on 30/09/2017.
+ */
+
+public class CVLFragment extends Fragment {
+
+    WebView Wview;
+    Database db;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_train, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        getActivity().setTitle(R.string.optymo);
+        db = new Database(getActivity());
+        db.open();
+        Wview = view.findViewById(R.id.web_view_train);
+        Wview.loadUrl("file:///"+getActivity().getApplicationContext().getFilesDir().toString()+"/"+db.timestamp("cvl"));
+        db.close();
+    }
+}
