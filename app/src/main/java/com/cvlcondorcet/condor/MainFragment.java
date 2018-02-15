8c2b5468beae72da2855b9a7115785ec34d41930
Quentin DE MUYNCK <quentin.demuynck@outlook.com>
2018-02-15 08:39:52 +0100

    Creating new branch, starting developing Deep Linking

--- app/src/main/java/com/cvlcondorcet/condor/MainFragment.java
+++ app/src/main/java/com/cvlcondorcet/condor/MainFragment.java
@@ -8,7 +8,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -247,7 +246,6 @@ public class MainFragment extends Fragment {
                 toDisplay = element2.toString();
                 toDisplay +="<style>.carousel { background: transparent; width: 100%; margin: auto;} head, body, div { background: transparent; margin: auto;}</style>";
                 toDisplay += element.toString();
-                Log.i("test", element.toString());
             } catch (IOException e) {}
             return null;
         }
