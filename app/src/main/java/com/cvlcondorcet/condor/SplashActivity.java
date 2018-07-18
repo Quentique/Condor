2d32a2ef826212588722e0686039c5cecd4be38a
Quentin DE MUYNCK <quentin.demuynck@outlook.com>
2018-07-18 09:05:08 +0200

    Fixing bugs (Thrown Exceptions)

--- app/src/main/java/com/cvlcondorcet/condor/SplashActivity.java
+++ app/src/main/java/com/cvlcondorcet/condor/SplashActivity.java
@@ -20,45 +20,48 @@ public class SplashActivity extends AppCompatActivity {
         Log.i("OPENING", "SPLASH ACITIVTY");
         try {
             List<String> list = getIntent().getData().getPathSegments();
+            if (!list.isEmpty()) {
+                switch (list.get(0)) {
+                    case "posts":
+                        if (!list.get(1).equals("")) {
+                            Intent intent2 = new Intent(this, PostViewerActivity.class);
+                            intent2.putExtra("id", list.get(1));
+                            startActivity(intent);
+                            startActivity(intent2);
+                        }
+                        break;
+                    case "events":
+                        if (!list.get(1).equals("")) {
+                            Intent intent2 = new Intent(this, EventViewerActivity.class);
+                            intent2.putExtra("id", list.get(1));
+                            startActivity(intent);
+                            startActivity(intent2);
+                        }
+                        break;
+                    case "maps":
+                        if (!list.get(1).equals("")) {
+                            intent.putExtra("fragment", "maps");
+                            intent.putExtra("place", list.get(1));
+                            intent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_DEBUG_LOG_RESOLUTION);
+                            startActivity(intent);
 
-            switch (list.get(0)) {
-                case "post":
-                    if (!list.get(1).equals("")) {
-                        Intent intent2 = new Intent(this, PostViewerActivity.class);
-                        intent2.putExtra("id", list.get(1));
-                        startActivity(intent);
+                            //NavUtils.navigateUpTo(this, intent);
+                        }
+                        break;
+                    case "cgu":
+                        Intent intent2 = new Intent(this, LicensesActivity.class);
+                        intent2.putExtra("name", "cgu");
+                        Log.i("TEST", "CGU");
                         startActivity(intent2);
-                    }
-                    break;
-                case "event":
-                    if (!list.get(1).equals("")) {
-                        Intent intent2 = new Intent(this, EventViewerActivity.class);
-                        intent2.putExtra("id", list.get(1));
-                        startActivity(intent);
-                        startActivity(intent2);
-                    }
-                    break;
-                case "maps":
-                    if(!list.get(1).equals("")) {
-                        intent.putExtra("fragment", "maps");
-                        intent.putExtra("place", list.get(1));
-                        intent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_DEBUG_LOG_RESOLUTION);
-                        startActivity(intent);
+                        break;
 
-                        //NavUtils.navigateUpTo(this, intent);
-                    }
-                    break;
-                case "cgu":
-                    Intent intent2 = new Intent(this, LicensesActivity.class);
-                    intent2.putExtra("name", "cgu");
-                    Log.i("TEST", "CGU");
-                    startActivity(intent2);
-                    break;
-
-                default:
-                    Log.i("TEST", "DEFAULT CASE");
-                    startActivity(intent);
-                    break;
+                    default:
+                        Log.i("TEST", "DEFAULT CASE");
+                        startActivity(intent);
+                        break;
+                }
+            } else {
+                startActivity(intent);
             }
         } catch (NullPointerException e) {
             Log.i("ERROR", "CATCH EXCEPTION");
