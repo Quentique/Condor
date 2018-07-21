182bcc868597ee40ce65f9ac057eb7e2bc75e7ac
Quentin DE MUYNCK <quentin.demuynck@outlook.com>
2018-07-21 07:39:52 +0200

    Actualising notifications count when sync's done when app runs

--- app/src/main/java/com/cvlcondorcet/condor/Sync.java
+++ app/src/main/java/com/cvlcondorcet/condor/Sync.java
@@ -124,6 +124,10 @@ public class Sync extends IntentService {
     @Override
     public void onHandleIntent(Intent i)
     {
+        String act = "none";
+        if (i.getExtras() != null && i.getExtras().containsKey("from")) {
+            act = i.getStringExtra("from");
+        }
         progressMessage = "Syncing...";
         //Log.i("NOTI", "DDDDDONE");
         CharSequence tickerText = getString(R.string.sync_notif_name);
@@ -175,6 +179,7 @@ public class Sync extends IntentService {
         if (!networkError) {
             try {
                 db.open();
+                db.initialiseSync();
                // Log.i("SYNC", "GENERAL SYNC");
                 JSONArray maps = get(MAPS_URL);
                 db.updateMaps(maps);
@@ -208,6 +213,14 @@ public class Sync extends IntentService {
                 noti.setProgress(100, 100, false);
                 noti.setContentText(getString(R.string.sync_end));
                 manager.notify(5, noti.build());
+                Log.i("SYNC", "ABOUT TO CALL CONFIGURATION");
+
+                if (db.endingSync() && act.equals("activity")) {
+                    Log.i("SYNC", "CALLED CONFIGURATION");
+                    Intent restart = new Intent(this, MainActivity.class);
+                    restart.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(restart);
+                }
                 db.close();
             } catch(SQLException e){
                 progress = -2;
