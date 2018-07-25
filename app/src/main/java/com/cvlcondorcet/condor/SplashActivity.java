3ba458902c0d653f465000fdb369c7b8cc03f1e2
Quentin DE MUYNCK <quentin.demuynck@outlook.com>
2018-07-25 11:38:31 +0200

    Added User Consent Activity :  - IntroActivity use  - Saving choices in Preferences  - Dislaying choices in PreferencesFragment

--- app/src/main/java/com/cvlcondorcet/condor/SplashActivity.java
+++ app/src/main/java/com/cvlcondorcet/condor/SplashActivity.java
@@ -2,6 +2,7 @@ package com.cvlcondorcet.condor;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -17,60 +18,64 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        intent = new Intent(this, MainActivity.class);
-        Log.i("OPENING", "SPLASH ACITIVTY");
-        try {
-            List<String> list = getIntent().getData().getPathSegments();
-            if (!list.isEmpty()) {
-                switch (list.get(0)) {
-                    case "posts":
-                        if (!list.get(1).equals("")) {
-                           launchPosts(list.get(1));
-                        }
-                        break;
-                    case "events":
-                        if (!list.get(1).equals("")) {
-                           launchEvents(list.get(1));
-                        }
-                        break;
-                    case "maps":
-                        if (!list.get(1).equals("")) {
-                            intent.putExtra("fragment", "maps");
-                            intent.putExtra("place", list.get(1));
-                            intent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_DEBUG_LOG_RESOLUTION);
+        if (PreferenceManager.getDefaultSharedPreferences(this).getInt("version",0) < BuildConfig.VERSION_CODE) {
+            intent = new Intent(this, ConsentActivity.class);
+        } else {
+            intent = new Intent(this, MainActivity.class);
+            Log.i("OPENING", "SPLASH ACITIVTY");
+            try {
+                List<String> list = getIntent().getData().getPathSegments();
+                if (!list.isEmpty()) {
+                    switch (list.get(0)) {
+                        case "posts":
+                            if (!list.get(1).equals("")) {
+                                launchPosts(list.get(1));
+                            }
+                            break;
+                        case "events":
+                            if (!list.get(1).equals("")) {
+                                launchEvents(list.get(1));
+                            }
+                            break;
+                        case "maps":
+                            if (!list.get(1).equals("")) {
+                                intent.putExtra("fragment", "maps");
+                                intent.putExtra("place", list.get(1));
+                                intent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_DEBUG_LOG_RESOLUTION);
+                                startActivity(intent);
+
+                                //NavUtils.navigateUpTo(this, intent);
+                            }
+                            break;
+                        case "cgu":
+                            Intent intent2 = new Intent(this, LicensesActivity.class);
+                            intent2.putExtra("name", "cgu");
+                            Log.i("TEST", "CGU");
+                            startActivity(intent2);
+                            break;
+
+                        default:
+                            Log.i("TEST", "DEFAULT CASE");
                             startActivity(intent);
-
-                            //NavUtils.navigateUpTo(this, intent);
-                        }
-                        break;
-                    case "cgu":
-                        Intent intent2 = new Intent(this, LicensesActivity.class);
-                        intent2.putExtra("name", "cgu");
-                        Log.i("TEST", "CGU");
-                        startActivity(intent2);
-                        break;
-
-                    default:
-                        Log.i("TEST", "DEFAULT CASE");
-                        startActivity(intent);
-                        break;
-                }
-            } else {
-                startActivity(intent);
-            }
-        } catch (NullPointerException e) {
-            Log.i("ERROR", "CATCH EXCEPTION");
-            if (getIntent().getExtras() != null ) {
-                if (getIntent().getExtras().containsKey("posts")) {
-                    launchPosts(getIntent().getExtras().getString("posts"));
-                } else if (getIntent().getExtras().containsKey("events")) {
-                    launchEvents(getIntent().getExtras().getString("events"));
+                            break;
+                    }
                 } else {
-                    intent.putExtras(getIntent().getExtras());
                     startActivity(intent);
                 }
-            } else {
-                startActivity(intent);
+            } catch (NullPointerException e) {
+                Log.i("ERROR", "CATCH EXCEPTION");
+                if (getIntent().getExtras() != null) {
+                    if (getIntent().getExtras().containsKey("posts")) {
+                        launchPosts(getIntent().getExtras().getString("posts"));
+                    } else if (getIntent().getExtras().containsKey("events")) {
+                        launchEvents(getIntent().getExtras().getString("events"));
+                    } else {
+                        intent.putExtras(getIntent().getExtras());
+                        startActivity(intent);
+                    }
+                } else {
+                    startActivity(intent);
+                }
             }
         }
     }
