b91bbd9477f9f3dd2826d8428d16f837d5e60ea1
Quentin DE MUYNCK <quentin.demuynck@outlook.com>
2018-08-17 19:49:35 +0200

    Preparing for release

--- app/src/main/java/com/cvlcondorcet/condor/MainActivity.java
+++ app/src/main/java/com/cvlcondorcet/condor/MainActivity.java
@@ -30,6 +30,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -69,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i("CONDOR", "hello");
 
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         mFirebaseRemoteConfig.setDefaults(R.xml.defaults_remote_param);
